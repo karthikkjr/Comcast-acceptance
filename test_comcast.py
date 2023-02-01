7a42fbbf460bbf6e1174142f61ce5cb51af2a0ec
Kathik <karthik.kjr@gmail.com>
2023-02-01 13:09:10 +0530

    updates

--- test_comcast.py
+++ test_comcast.py
@@ -1,12 +1,15 @@
-from seleniumbase import BaseCase
-import requests, json
-from get_forecast import GetForeCast
+import requests
 from bs4 import BeautifulSoup
+from seleniumbase import BaseCase
+
+from get_forecast import GetForeCast
+
 BaseCase.main(__name__, __file__)
 
 
 class ComCast(BaseCase, GetForeCast):
     date_and_difference = None
+
     def test_scenario1(self):
         date_and_difference = self.get_current_date_and_days_difference(place="Kolkata", future_date="2023-02-05")
         print(f"Local Time: {date_and_difference.__getitem__(0)} Days Difference: {date_and_difference.__getitem__(1)}")
@@ -16,8 +19,7 @@ class ComCast(BaseCase, GetForeCast):
         days = str(date_and_difference.__getitem__(1))
         key = "e45c365fbbdf4aec84e220319233101"
         city = "Chennai"
-        url = "https://api.weatherapi.com/v1/forecast.json?"
-        req_url = url + "key=" + key + "&q=" + city + "&days=" + days + "&aqi=no&alerts=no"
+        req_url = f"https://api.weatherapi.com/v1/forecast.json?key={key}&q={city}&days={days}&aqi=no&alerts=no"
         response = requests.get(req_url)
         data = response.content
         soup = BeautifulSoup(data, 'html.parser')
